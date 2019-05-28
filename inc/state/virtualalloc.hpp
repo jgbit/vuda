fdf977bb79ddb4e4560badb877928cc27da4edc7
Yining Karl Li <betajippity@gmail.com>
2019-05-28 15:24:16 -0700

    Add support for MoltenVk on MacOS/iOS
    After the previous two commits, the only change needed
    is to detect Darwin-based systems using the __APPLE__
    preprocessor directive.

--- inc/state/virtualalloc.hpp
+++ inc/state/virtualalloc.hpp
@@ -3,7 +3,7 @@
 //
 // https://stackoverflow.com/questions/142508/how-do-i-check-os-with-a-preprocessor-directive/8249232
 
-#if defined(__linux__) || defined(__unix__)
+#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
 #define PLATFORM_NAME VUDA_UNIX
 #include <unistd.h>
 #include <string.h>
